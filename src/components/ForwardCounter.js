fcdffc67f16c5f3d102d1ea08787f1294487cf69
Sunny-jinn <rlawlsdn316@gmail.com>
2022-01-19 11:39:23 +0900

    Part. 1

--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -1,17 +1,8 @@
-import { useState, useEffect } from 'react';
-
 import Card from './Card';
+import useCounter from '../hooks/use_counter';
 
 const ForwardCounter = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  const counter = useCounter(); //논리만 공유하는 것이지 상태까지 공유되진 않음.
 
   return <Card>{counter}</Card>;
 };
